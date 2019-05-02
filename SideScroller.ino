06f65af6b433417a961be76054189042446c3ce3
heatharrose <50159223+heatharrose@users.noreply.github.com>
2019-05-02 09:11:46 -0400

    Update SideScroller.ino

--- SideScroller.ino
+++ SideScroller.ino
@@ -1,14 +1,12 @@
-#include <Arduboy2.h>
+ #include <Arduboy2.h>
 #include "World.h"
 
 Arduboy2 arduboy;
 
-#define playerJump (-100)
-byte playerFrame = 0;
 int playerX = 62;
 int playerY = 58;
 int playerVelocityY = 0;
-
+int worldX = 0;
 
 constexpr int playerWidth = 4;
 constexpr int playerHeight = 4;
@@ -27,58 +25,99 @@ void setup()
 
 }
 
-void loop() 
-{
- if(!arduboy.nextFrame())
- return;
-//checks button status
-arduboy.pollButtons();
+void loop() {
 
-  if(arduboy.pressed(LEFT_BUTTON))
-  {
-    playerX -= 1;
+  if(!arduboy.nextFrame())
+  return;
+  //checks button status
+  arduboy.pollButtons();
+
+  if(arduboy.pressed(LEFT_BUTTON)) {
+    if (worldX == 0) { 
+      if (playerX > 0) playerX -= 1;  
+      }
+    else if (worldX == (mapWidth - 16) * 8) { 
+      if (playerX > 64 - (playerWidth /2)) { 
+        playerX -= 1;
+      }
+      else {
+        worldX -= 1; 
+      }
+    }
+    else {
+        worldX -= 1;  
+    }
+
   }
-if(arduboy.pressed(RIGHT_BUTTON))
+
+if(arduboy.pressed(RIGHT_BUTTON)) {
+  if (worldX == 0) {
+    if (playerX < 64 - (playerWidth /2)) {
+      playerX += 1;
+    } else {
+      worldX += 1;
+    }
+  } else if (worldX == (mapWidth - 16) * 8) {
+    if (playerX < 128 - playerWidth) {
+      playerX += 1;
+    }
+  } else {
+    worldX += 1;
+  }
+}
+if(arduboy.justPressed(A_BUTTON))
+{//if player is not jumping
+  if(onGround)
+  {//make player jump
+    playerVelocityY = -2;
+    
+    onGround = false;
+
+  }
+}
+//if player is jumping
+if(playerVelocityY < 0)
+{//start gravity
+ if(arduboy.everyXFrames(gravityFrameRate))
+ {
+  playerVelocityY += gravity;
+ }
+}
+
+if(playerVelocityY >= 0)
 {
-  playerX += 1;
+  playerVelocityY = gravity;
+}
+int nextPlayerY = (playerY + playerVelocityY);
+int nextPlayerYBottom = (nextPlayerY + playerHeight);
+
+if(nextPlayerYBottom >= groundLevel) {
+
+  nextPlayerY = (groundLevel - playerHeight);
+  onGround = true;
+
 }
 
-bool jumpPressed() {
-return (arduboy.buttonsState() & (A_BUTTON)) != 0;
-}
+playerY = nextPlayerY;
 
-if(jumpPressed())
-{
-  startJump();
-}
-
-if ((onGround == true) and jumpPressed()) {
-  startJump();
-}
-
-
-  
 arduboy.clear();
 
+// Render the world ..
+
 for(uint8_t y = 0; y < mapHeight; ++y) {
+
   for(uint8_t x = 0; x < mapWidth; ++x) {
-    Sprites::drawOverwrite(x * tileWidth, y * tileHeight, ground,getMapTile(x, y));
+
+
+    Sprites::drawOverwrite(x * tileWidth - worldX, y * tileHeight, ground,getMapTile(x, y));
     
   }
   
-//draw player
-arduboy.fillRect(playerX, playerY, playerWidth, playerHeight, WHITE);
+  //draw player
+  arduboy.fillRect(playerX, playerY, playerWidth, playerHeight, WHITE);
 
+  
+  }
+  arduboy.display();
 
-}
-void startJump() {
-  playerVelocityY = playerJump;
-  playerYi = playerY;
-}
-
-void start_Gravity() {
-  playerYi = playerY;
-  ballV = 0;
-}
-arduboy.display();
 }
